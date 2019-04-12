9f7f06a2e953fe090af7145e34d15d0f8bf56dfb
cairc <cairc@yonyou.com>
2019-04-12 09:50:38 +0800

    U 配置修改

--- dev/js/index.js
+++ dev/js/index.js
@@ -174,7 +174,7 @@ $(document).ready(function(){
             $('.icon-search').css('color', '#CAD3DC');
         }
 
-        $.getJSON('../../search.json').done(function(data) {
+        $.getJSON('search.json').done(function(data) {
             var html = '';
             for (var i in data) {
                 var item = data[i];
